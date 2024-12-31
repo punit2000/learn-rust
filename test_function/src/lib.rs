a99a685653eafe0ed5371eb637a8f76f02a2290e
Punit Savlesha <Punit.savlesha@in.ey.com>
2024-12-31 13:24:33 +0530

    Day 11 [automated tests]

---
+++ test_function/src/lib.rs
@@ -0,0 +1,131 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub fn add_two(a: usize) -> usize {
+    a + 3
+}
+
+// pub fn greeting(name: &str) -> String {
+//     format!("Hello {name}!")
+// }
+
+pub fn greeting(name: &str) -> String {
+    String::from("Hello!")
+}
+
+pub struct Guess {
+    value: i32
+} 
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("guess value should be greater than or equal to 1, got {value}");
+        } else if value > 100 {
+            panic!("guess value should be less than or equal to 100, got {value}" );
+        }
+
+        Guess {value}
+    }
+}
+
+fn internal_adder(left: usize, right:usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    // #[test]
+    // fn it_adds_two() {
+    //     let result = add_two(2);
+    //     assert_eq!(result, 4);
+    // }
+
+    #[test]
+    #[ignore]
+    fn another(){
+        panic!("make this fail")
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"),
+        "Greeting did not contain name, value was `{result}`"
+        );
+    }
+    
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        let result = add(2, 2);
+
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    fn internal() {
+        let result = internal_adder(2,2);
+        assert_eq!(result,4);
+    }
+
+}
---
+++ test_function/tests/integration_tests.rs
@@ -0,0 +1,12 @@
+use test_function::add_two;
+
+mod common;
+
+#[test]
+
+fn it_adds_two() {
+    common::setup();
+    
+    let result = add_two(2);
+    assert_eq!(result,4);
+}
