8c1a9388f1a0a1fdcbc6713a21c6beab44fed424
Punit Savlesha <Punit.savlesha@in.ey.com>
2025-01-01 15:20:04 +0530

    Day 12 [added test cases, sensitive and insensitive search function]

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -11,17 +11,17 @@ fn main() {
 
     // let (query, file_path) = parse_config(&args);
     let config =   Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing the arguments: {err}");
+        eprintln!("Problem parsing the arguments: {err}");
         process::exit(1);
     });
 
 
 
-    println!("Searching for the query {}", config.query);
-    println!("In file {}", config.file_path);
+    println!("Searching for the query '{}'", config.query);
+    println!("In file '{}'", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 
