67ab8f9ed1f41fdc3c7e94c41bbc98a97d695bbb
Punit Savlesha <Punit.savlesha@in.ey.com>
2025-01-04 22:18:25 +0530

    Day 15 [more iterators and  workspaces]

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,14 +3,14 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
     // let query = &args[1];
     // let file_path = &args[2];
 
     // let (query, file_path) = parse_config(&args);
-    let config =   Config::build(&args).unwrap_or_else(|err| {
+    let config =   Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing the arguments: {err}");
         process::exit(1);
     });
